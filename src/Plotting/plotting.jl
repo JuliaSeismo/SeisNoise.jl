c97920f14a98a7e33c989111e789b8f453fc06d7
Tim Clements <thclements@g.harvard.edu>
2019-09-27 14:13:42 -0400

    Remove plot in favor of corrplot to minimize conflict with Plots

--- src/Plotting/plotting.jl
+++ src/Plotting/plotting.jl
@@ -1,14 +1,17 @@
-export plot
+export corrplot
 
 """
 
-  plot(C)
+  corrplot(C)
 
 Plot a CorrData matrix.
 """
-function plot(C::CorrData;kwargs...)
+function corrplot(C::CorrData)
     lags = -C.maxlag:1/C.fs:C.maxlag
-    times = timestamp.(C.t)
-    heatmap(lags,times,C.corr',c=:RdBu,kwargs...)
-    xlabel!("Lags [s]")
+    times = Dates.format.(Dates.unix2datetime.(C.t),"yyyy/m/d HH:MM")
+    Cstack = stack(C,allstack=true)
+    plot(
+         heatmap(lags,times,C.corr',c=:balance,legend=:none),
+         plot(lags,Cstack.corr,c=:black,linewidth=1.5,legend=:none,xlabel="Lag [s]"),
+         layout = grid(2,1,heights=[0.75,0.25]), link=:x,dpi=1000)
 end
